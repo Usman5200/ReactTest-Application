78c7729ccb4e5fb805d0ddf8c90e5cb9a1950cf2
Usman Khalid <usmankoreshi@gmail.com>
2022-08-30 19:01:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React ,{Component} from 'react';
+import Navbar from './Components/Navbar';
+import Calendar from './Components/Calender';
+import Content from './Components/Content';
+import Images from './Components/Images';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+
+
+export default  class App extends Component{
+  render(){
+ 
+return(
+  <div>
+    <Navbar/>
+    <Content/>
+    <Images/>
+    <Calendar
+      />
+  
+    
+
+    
+  </div>
+)
+  }
+    
 }
 
-export default App;
+
